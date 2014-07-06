8e2db346bf1ffc969c641e5960b31b96324a84ae
arvdroid <amudun@gmail.com>
2014-07-05 21:26:41 -0700

    submitting project files

--- src/com/codepath/healthpact/activity/LoginActivity.java
+++ src/com/codepath/healthpact/activity/LoginActivity.java
@@ -15,7 +15,6 @@ import com.codepath.oauth.OAuthLoginActivity;
 import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
